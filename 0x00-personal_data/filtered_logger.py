a187b55acfda638a7624dc71b8dd57740bc9ddbe
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-10 11:04:38 +0100

    created a get db connect obj

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -79,7 +79,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db()-> mysql.connector:
     """function that returns a connector to the database
     (mysql.connector.connection.MySQLConnection object).
 
