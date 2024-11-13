471f0daadc1206e6ad4d4737dc69569b65629668
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-13 16:49:54 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-"""Basic Athentication
+"""Create a class BasicAuth that inherits from Auth. For the moment this
+class will be empty. Update api/v1/app.py for using BasicAuth class instead
+of Auth depending of the value of the environment variable
+AUTH_TYPE, If AUTH_TYPE is equal to basic_auth:
+
+import BasicAuth from api.v1.auth.basic_auth
+create an instance of BasicAuth and assign it to the variable auth
 """
 from .auth import Auth
 
