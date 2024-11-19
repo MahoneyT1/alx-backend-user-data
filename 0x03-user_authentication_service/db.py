36d76074d6a3650262d9ab41bdf5c2e7ac140ecc
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-19 16:39:07 +0100

    find user by attribute email

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+import logging
+
+
+logging.disable(logging.CRITICAL)
 
 
 class DB:
@@ -43,3 +48,19 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """This method takes in arbitrary keyword arguments and returns
+        the first row found in the users table as filtered by the method’s
+        input arguments
+        """
+        try:
+            # if not kwargs:
+            #     raise InvalidRequestError("No filter criteria provided.")
+            result = self._session.query(User).filter_by(**kwargs).first()
+
+            if result is None:
+                raise NoResultFound
+            return result
+        except InvalidRequestError:
+            raise
