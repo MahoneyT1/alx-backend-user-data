b4fd4fe0c4e9025204c68a2fbf116250ecdbd6e8
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-19 16:44:03 +0100

    find user by attribute email

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,7 +50,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: Dict)-> int:
+    def find_user_by(self, **kwargs: Dict) -> int:
         """This method takes in arbitrary keyword arguments and returns
         the first row found in the users table as filtered by the method’s
         input arguments
