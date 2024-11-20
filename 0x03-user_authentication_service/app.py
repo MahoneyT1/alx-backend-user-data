d1b40b1fc218272eef4e107750bc9070a9cb8ee5
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-20 11:04:18 +0100

    created user with our auth system

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ A Simple flask app"""
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request, abort
 from typing import Dict
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -14,5 +16,21 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_user():
+    """Register User route
+    """
+    # extract email and password from request form
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({'email': user.email,
+                        "message": "user created"
+                        }), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000, debug=True)
